2eed8d308e58303002afd6aa5b7461a7a0043f33
jkim0120 <joskim0120@gmail.com>
2016-05-03 01:32:52 -0400

    Implement custom server

--- http_server/http.go
+++ http_server/http.go
@@ -5,12 +5,31 @@ import (
   "net/http"
 )
 
+type myHandler struct{}
+
+func (*myHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+  if handler, ok := mux[r.URL.String()]; ok {
+    handler(w, r)
+    return
+  }
+
+  io.WriteString(w, "My server: " + r.URL.String())
+}
+
+var mux map[string]func(http.ResponseWriter, *http.Request)
+
 func handler(w http.ResponseWriter, r *http.Request) {
   io.WriteString(w, "Hello World!")
 }
 
 func main() {
-  mux := http.NewServeMux()
-  mux.HandleFunc("/", handler)
-  http.ListenAndServe(":8080", mux)
+  server := http.Server {
+    Addr: ":8080",
+    Handler: &myHandler{},
+  }
+
+  mux := make(map[string]func(http.ResponseWriter, *http.Request))
+  mux["/"] = handler
+
+  server.ListenAndServe()
 }
